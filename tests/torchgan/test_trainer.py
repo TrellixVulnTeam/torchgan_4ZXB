9150ba76f388b37e6e1092c26fefe5e954e76b73
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-23 03:55:29 +0000

    Adding tarfile member sanitization to extractall()

--- tests/torchgan/test_trainer.py
+++ tests/torchgan/test_trainer.py
@@ -23,7 +23,26 @@ def mnist_dataloader():
     if not os.path.exists("MNIST"):
         filename = wget.download("http://www.di.ens.fr/~lelarge/MNIST.tar.gz")
         with tarfile.open(filename, "r:gz") as tar:
-            tar.extractall()
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar)
     train_dataset = dsets.MNIST(
         root="./",
         train=True,
